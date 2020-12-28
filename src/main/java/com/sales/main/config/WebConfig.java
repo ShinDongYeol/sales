e146af2a99b5d2b20ad629b39b9d43489685c619
ryudd93 <ryudd93@gmail.com>
2020-12-28 20:56:36 +0900

    cors 추가

--- src/main/java/com/sales/main/config/WebConfig.java
+++ src/main/java/com/sales/main/config/WebConfig.java
@@ -3,12 +3,21 @@ package com.sales.main.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	
+	  @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins("*")
+	                .allowedMethods("GET", "POST")
+	                .maxAge(3000);
+	}
 
     @Bean
 
