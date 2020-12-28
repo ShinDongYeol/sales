2ccbb5786a2409fe7564abd4e96008e6c901c5d2
sdy922 <sdy922@jasongroup.co.kr>
2020-12-28 18:24:24 +0900

    개발중

---
+++ src/main/java/com/sales/main/mapper/login/LoginMapper.java
@@ -0,0 +1,9 @@
+package com.sales.main.mapper.login;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface LoginMapper {
+}
