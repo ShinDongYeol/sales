cf377bd0b066ebf9d8c030c42600191057358afe
sdy922 <sdy922@jasongroup.co.kr>
2020-12-31 18:16:53 +0900

    ll

---
+++ src/main/java/com/sales/main/mapper/join/JoinMapper.java
@@ -0,0 +1,9 @@
+package com.sales.main.mapper.join;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface JoinMapper {
+}
